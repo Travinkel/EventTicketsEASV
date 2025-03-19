c6d6aaf3ffdb5d9fe2466cb33352a21fb0a7c9ab
Travinkel <ankersoe@protonmail.com>
2025-03-19 12:45:15 +0100

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,9 +3,13 @@ module org.example.eventticketsystem {
     requires javafx.fxml;
     requires javafx.swing;
 
-
     opens org.example.eventticketsystem to javafx.fxml;
+    opens org.example.eventticketsystem.controllers to javafx.fxml;
+    opens org.example.eventticketsystem.utils to javafx.fxml;
+
     exports org.example.eventticketsystem;
     exports org.example.eventticketsystem.controllers;
-    opens org.example.eventticketsystem.controllers to javafx.fxml;
+    exports org.example.eventticketsystem.utils;
+    exports org.example.eventticketsystem.services;
+    exports org.example.eventticketsystem.models;
 }
